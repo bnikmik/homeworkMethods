72c2995c5493569d96279e17faaca6f3fc5963ae
Nikita Bolshakov <bnikmik@gmail.com>
2022-07-04 10:27:13 +0300

    Домашнее задание: Методы.(поправил код)

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,50 @@
 import java.time.LocalDate;
 
 public class Main {
-    public static void checkLeapYear (int a){
-        if ((a % 100 != 0 || a % 400 == 0) && (a % 4 == 0)) {
-            System.out.println(a + " год високосный");
+    public static void checkLeapYear (int year){
+        if ((year % 100 != 0 || year % 400 == 0) && (year % 4 == 0)) {
+            System.out.println(year + " год високосный");
         }
-        else System.out.println(a + " год не является високосным");
+        else System.out.println(year + " год не является високосным");
     }
 
-    public static void checkClientOS (int a,int b) {
+    public static void checkClientOS (int clientOS,int clientDeviceYear) {
         int currentYear = LocalDate.now().getYear();
-        if (a == 0){
-            if (b<currentYear) {
-                System.out.println("Установите  lite-версию iOS");
-            }
-            else System.out.println("Установите  обычную версию iOS");
+        if (clientOS == 0 && clientDeviceYear < currentYear){
+            System.out.println("Установите  lite-версию iOS");
         }
-        if (a == 1) {
-            if (b<currentYear) {
-                System.out.println("Установите  lite-версию Android");
-            }
-            else System.out.println("Установите  обычную версию Android");
+        else if (clientOS == 0 && clientDeviceYear == currentYear) {
+            System.out.println("Установите  обычную версию iOS");
         }
-
+        else if (clientOS == 1 && clientDeviceYear < currentYear){
+            System.out.println("Установите  lite-версию Android");
+        }
+        else {
+            System.out.println("Установите  обычную версию Android");
+        }
     }
-    public static int checkCountDay(int deliveryDistance) {
 
+    public static int checkCountDay(int deliveryDistance) {
         int distanceFree = 20;
         int distanceStep = 40;
-
         return ((deliveryDistance - distanceFree) / distanceStep) + 2;
     }
-    public static void main(String[] args) {
 
-        int year = 2001;
-        checkLeapYear(year);
-
-        int clientOS = 0;
-        int clientDeviceYear = 2022;
-        checkClientOS(clientOS,clientDeviceYear);
-
-        int deliveryDistance = 95;
-        int countDay = checkCountDay(deliveryDistance);
+    public static void displayCountDay (int countDay,int deliveryDistance) {
         if (deliveryDistance < 20) {
             System.out.println("Для доставки потребуется дней: 1");
         }
         else System.out.println("Для доставки потребуется дней: " + countDay);
+    }
 
+    public static void main(String[] args) {
+        int year = 2001;
+        checkLeapYear(year);
+        int clientOS = 1;
+        int clientDeviceYear = 2020;
+        checkClientOS(clientOS,clientDeviceYear);
+        int deliveryDistance = 20;
+        int countDay = checkCountDay(deliveryDistance);
+        displayCountDay(countDay,deliveryDistance);
     }
 }
